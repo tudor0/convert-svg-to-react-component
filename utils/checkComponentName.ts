e181e8d0acc5c8fcd714afdec4bf724e8303d5d4
Tudor <tudor.tuglea@gmail.com>
2023-02-09 23:10:06 +0200

    added examples

--- utils/checkComponentName.ts
+++ utils/checkComponentName.ts
@@ -1,4 +1,6 @@
 const checkIfNameIsValid = (componentName: string) => {
+  // Starts with capital letter
+  // Contains only letters, numbers and underscores, but no numbers and underscores at the start
   const regex = /^[A-Z][a-zA-Z0-9_]*[^_]$/;
   return regex.test(componentName);
 };
