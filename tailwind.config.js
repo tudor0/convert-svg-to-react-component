6b943a7d8ff25f5935d1cfd3f557d81411448951
Tudor <tudor.tuglea@gmail.com>
2023-03-01 21:25:38 +0200

    added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./pages/**/*.tsx", "./components/**/*.tsx"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        black: "#010400",
+        blackOlive: "#30332E",
+        snow: "#FFFBFC",
+        verdigris: "#62BBC1",
+        cerise: "#EC058E"
+      }
+    }
   },
   plugins: []
 };
